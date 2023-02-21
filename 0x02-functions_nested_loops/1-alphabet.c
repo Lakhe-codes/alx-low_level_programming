df93c2d3bf9694d8891bfc5e44fbd3a12319350a
Lakhe <Mandilakhematyana@gmail.com>
2023-02-21 13:56:38 -0800

    Yeha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+_putchar('a');
+putchar('b');
+}
+int main(void)
+{
+print_alphabet();
+return (0);
+}
