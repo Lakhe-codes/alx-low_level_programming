94c3cb01833140e1769e25539a0afa07fb69fff3
Lakhe <Mandilakhematyana@gmail.com>
2023-02-22 03:44:38 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
- * main - check the code.
+ * print_alphabet_x10 - prints alphabet ten times.
  *
- * print_alphabet_x10 - prints alphabet x10.
- * 
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
