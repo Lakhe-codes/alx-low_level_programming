cae93ebae14e5267bb74a569cf4965301625ff35
Lakhe <Mandilakhematyana@gmail.com>
2023-02-24 10:11:06 -0800

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN-H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* main.h */
+#endif
